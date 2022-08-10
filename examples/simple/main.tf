b667af4a7dfbc2bb68c5dbfcf8a4285b97d78983
Mya <j.mya.pitz@gmail.com>
2022-08-10 10:00:05 -0500

    spruce up README

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,10 +1,5 @@
 terraform {
   required_providers {
-    helm = {
-      source = "hashicorp/helm"
-      version = "~> 2.0"
-    }
-
     storj = {
       source  = "terraform.local/local/storj"
       version = "1.0.0"
